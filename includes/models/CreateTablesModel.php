a6de95eac55d4f7e7c3e47884bcf72da6d7bc1f3
elauser <1810653024@stud.fh-kufstein.ac.at>
2019-04-30 09:13:32 +0200

    Models Test

---
+++ includes/models/CreateTablesModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 29.04.2019
+ * Time: 20:47
+ */
+
+class CreateTablesModel
+{
+
+}
---
+++ includes/models/SummonerModel.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 28.04.2019
+ * Time: 22:07
+ */
