68d38739386c339d594cd21d96164d324877b977
Julian Loferer <julian@loferer.info>
2019-05-02 00:59:33 +0200

    Fixed Matchlist Error on no Matches found

--- includes/controllers/IndexController.php
+++ includes/controllers/IndexController.php
@@ -148,9 +148,11 @@ class IndexController extends Controller
                 if ($matchListDb == null) {
                     // Hole MatchList von Externer API da nicht in DB vorhanden
                     $matchList = $this->view->api->call('https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/' . $arr["accountId"]);
-                    foreach($matchList["matches"] as $match){
-                        if(MatchHistoryModel::createNewMatchHistory($match, $arr["accountId"])){
-                            break;
+                    if($matchList["matches"] != null){
+                        foreach($matchList["matches"] as $match){
+                            if(MatchHistoryModel::createNewMatchHistory($match, $arr["accountId"])){
+                                break;
+                            }
                         }
                     }
 
@@ -182,92 +184,96 @@ class IndexController extends Controller
                        <h5 class="pl-2 pt-2">Matchhistory:</h5>
                        <ul class="list-group list-group-flush bg-loldark">';
                         $i = 0;
-                        foreach ($matchList["matches"] as $match) {
-                            if ($i == 20) break;
-                            $matchArrDb = MatchesModel::getMatchesByGameId($match["gameId"]);
-                            $matchArr = $matchArrDb;
-                            $participantIdents = json_decode($matchArr["participantIdentities"], true);
-                            $participantsArr = json_decode($matchArr["participants"], true);
-                            $teamsArr = json_decode($matchArr["teams"], true);
-                            if($matchArr == null){
-                                $matchArr = $this->view->api->call('https://euw1.api.riotgames.com/lol/match/v4/matches/' . $match["gameId"]);
-                                MatchesModel::createNewMatches($matchArr);
-                                $participantIdents = $matchArr["participantIdentities"];
-                                $participantsArr = $matchArr["participants"];
-                                $teamsArr = $matchArr["teams"];
-                            }
-                            $participants = '';
-                            $teamId = 0;
-                            $champId = 0;
-                            $myChampName = '';
-                            $win = true;
-                            foreach ($participantIdents as $participant) {
-                                $championId = 0;
-                                $champName = "ZZZZ";
-                                foreach ($participantsArr as $participantDetail) {
-                                    // Hole TeamId und ChampId des Summoners
-                                    if (($participantDetail["participantId"] == $participant["participantId"]) && (strtolower($participant["player"]["summonerName"]) == strtolower($summonerClean))) {
-                                        $teamId = $participantDetail["teamId"];
-                                        $champId = $participantDetail["championId"];
-                                        break;
+                        if($matchList["matches"] != null) {
+                            foreach ($matchList["matches"] as $match) {
+                                if ($i == 20) break;
+                                $matchArrDb = MatchesModel::getMatchesByGameId($match["gameId"]);
+                                $matchArr = $matchArrDb;
+                                $participantIdents = json_decode($matchArr["participantIdentities"], true);
+                                $participantsArr = json_decode($matchArr["participants"], true);
+                                $teamsArr = json_decode($matchArr["teams"], true);
+                                if ($matchArr == null) {
+                                    $matchArr = $this->view->api->call('https://euw1.api.riotgames.com/lol/match/v4/matches/' . $match["gameId"]);
+                                    MatchesModel::createNewMatches($matchArr);
+                                    $participantIdents = $matchArr["participantIdentities"];
+                                    $participantsArr = $matchArr["participants"];
+                                    $teamsArr = $matchArr["teams"];
+                                }
+                                $participants = '';
+                                $teamId = 0;
+                                $champId = 0;
+                                $myChampName = '';
+                                $win = true;
+                                foreach ($participantIdents as $participant) {
+                                    $championId = 0;
+                                    $champName = "ZZZZ";
+                                    foreach ($participantsArr as $participantDetail) {
+                                        // Hole TeamId und ChampId des Summoners
+                                        if (($participantDetail["participantId"] == $participant["participantId"]) && (strtolower($participant["player"]["summonerName"]) == strtolower($summonerClean))) {
+                                            $teamId = $participantDetail["teamId"];
+                                            $champId = $participantDetail["championId"];
+                                            break;
+                                        }
+                                    }
+                                    // Hole ChampionId des jeweiligen Participant
+                                    foreach ($participantsArr as $participantDetail) {
+                                        if ($participantDetail["participantId"] == $participant["participantId"]) {
+                                            $championId = $participantDetail["championId"];
+                                            break;
+                                        }
+                                    }
+                                    // Hole sowohl Champion Name von Summoner als auch vom jeweiligen Participant
+                                    foreach ($champData["data"] as $champ) {
+                                        if ($champ["key"] == $champId) {
+                                            $myChampName = $champ["id"];
+                                        }
+                                        if ($champ["key"] == $championId) {
+                                            $champName = $champ["id"];
+                                            break;
+                                        }
+                                    }
+                                    $participantName = $participant["player"]["summonerName"];
+                                    if (strtolower($participantName) == strtolower($summonerClean)) {
+                                        $participantName = '<span style="color: #c7b184;">' . $participantName . '</span>';
+                                    }
+                                    $participants .= '<img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' . $champName . '.png" width="16px" height="16px" alt=" "/> ' . $participantName;
+                                }
+                                // Prüfe ob das aktuelle Game gewonnen ist
+                                foreach ($teamsArr as $team) {
+                                    if ($team["teamId"] == $teamId) {
+                                        if ($team["win"] === "Fail") {
+                                            $win = false;
+                                        }
                                     }
                                 }
-                                // Hole ChampionId des jeweiligen Participant
-                                foreach ($participantsArr as $participantDetail) {
-                                    if ($participantDetail["participantId"] == $participant["participantId"]) {
-                                        $championId = $participantDetail["championId"];
-                                        break;
-                                    }
+                                $winString = '<span class="text-success">WIN</span>';
+                                if (!$win) {
+                                    $winString = '<span class="text-danger">LOST</span>';
                                 }
-                                // Hole sowohl Champion Name von Summoner als auch vom jeweiligen Participant
-                                foreach ($champData["data"] as $champ) {
-                                    if ($champ["key"] == $champId) {
-                                        $myChampName = $champ["id"];
-                                    }
-                                    if ($champ["key"] == $championId) {
-                                        $champName = $champ["id"];
-                                        break;
-                                    }
-                                }
-                                $participantName = $participant["player"]["summonerName"];
-                                if(strtolower($participantName) == strtolower($summonerClean)){
-                                    $participantName = '<span style="color: #c7b184;">' . $participantName . '</span>';
-                                }
-                                $participants .= '<img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' . $champName . '.png" width="16px" height="16px" alt=" "/> ' . $participantName;
-                            }
-                            // Prüfe ob das aktuelle Game gewonnen ist
-                            foreach ($teamsArr as $team) {
-                                if ($team["teamId"] == $teamId) {
-                                    if ($team["win"] === "Fail") {
-                                        $win = false;
-                                    }
-                                }
-                            }
-                            $winString = '<span class="text-success">WIN</span>';
-                            if (!$win) {
-                                $winString = '<span class="text-danger">LOST</span>';
-                            }
 
-                            // Berechne Tage welche vergangen sind, seit das Game gespielt wurde
-                            $now = time();
-                            $gameDate = $matchArr["gameCreation"] / 1000;
-                            $datediff = $now - $gameDate;
-                            $days = round($datediff / (60 * 60 * 24));
-                            // ** Ende Berechnung **
-                            $dayString = '<span class="text-muted">Today</span>';
-                            if ($days > 0) {
-                                $dayString = '<span class="text-muted">' . $days . ' days ago</span>';
-                            }
+                                // Berechne Tage welche vergangen sind, seit das Game gespielt wurde
+                                $now = time();
+                                $gameDate = $matchArr["gameCreation"] / 1000;
+                                $datediff = $now - $gameDate;
+                                $days = round($datediff / (60 * 60 * 24));
+                                // ** Ende Berechnung **
+                                $dayString = '<span class="text-muted">Today</span>';
+                                if ($days > 0) {
+                                    $dayString = '<span class="text-muted">' . $days . ' days ago</span>';
+                                }
 
-                            $sumName = $arr["name"];
-                            if($sumName == null){
-                                $sumName = $arr["summonerName"];
-                            }
-                            $matchString = '<li class="list-group-item"><span class="text-warning">' . $matchArr["gameMode"] . '</span> ' . $dayString . ' ' . $winString . ' <span>
+                                $sumName = $arr["name"];
+                                if ($sumName == null) {
+                                    $sumName = $arr["summonerName"];
+                                }
+                                $matchString = '<li class="list-group-item"><span class="text-warning">' . $matchArr["gameMode"] . '</span> ' . $dayString . ' ' . $winString . ' <span>
                                     <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' . $myChampName . '.png" width="16px" height="16px" alt=" "/> ' . $sumName . '</span><br>
                                             <span class="float-right">' . $participants . '</span></li>';
-                            echo $matchString;
-                            $i++;
+                                echo $matchString;
+                                $i++;
+                            }
+                        } else {
+                            echo '<li class="list-group-item">Keine Matches vorhanden.</li>';
                         }
                         echo '</ul>
                     </div>
